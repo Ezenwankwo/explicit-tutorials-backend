25ff3678438511c406c8dab3a9c46faadfbbf246
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2022-11-24 13:46:34 +0100

    fix: setup precommit

--- config/settings/base.py
+++ config/settings/base.py
@@ -293,7 +293,10 @@ SPECTACULAR_SETTINGS = {
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
     "SERVERS": [
         {"url": "http://127.0.0.1:8000", "description": "Local Development server"},
-        {"url": "https://explicitonlinetutorials.com", "description": "Production server"},
+        {
+            "url": "https://explicitonlinetutorials.com",
+            "description": "Production server",
+        },
     ],
 }
 # Your stuff...
